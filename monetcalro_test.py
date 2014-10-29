e238dc1a18460e117e7bf769038ffff81e254038
Sherif <ucapsib@ucl.ac.uk>
2014-10-28 20:12:40 -0700

    initial commit

---
+++ monetcalro_test.py
@@ -0,0 +1,20 @@
+## test file for monte carlo method 
+
+from nose.tools import assert_true, assert_equal, assert_almost_equal, assert_not_equal
+    from my_module import montecarlo 
+	def test_montecarlo_test_returns_true():
+    argument = "Energy"
+    actual = montecarlo(Energy)
+    assert_true(actual, "The evaluated value of the Energy")
+	 def test_exception_is_raised():
+         from nose.tools import assert_raises
+         with assert_raises(10e-5) as exception:
+        raise ValueError("The value is wrong")
+
+    
+	
+	
+
+	
+	
+	
